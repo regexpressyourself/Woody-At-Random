7b9495868c2d876cc0b3c60092b2afad6b94c6eb
Sam Messina <samuel.messina@gmail.com>
2016-05-16 12:16:43 -0500

    added tags files

--- filmography.php
+++ filmography.php
@@ -12,14 +12,14 @@
 
           <!-- Bootstrap -->
           <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-            <link href="custom.css" rel="stylesheet">
-            <link href="flip.css" rel="stylesheet">
+          <link href="custom.css" rel="stylesheet">
+          <link href="flip.css" rel="stylesheet">
 
-              <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-                            <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-                                                        <!--[if lt IE 9]>
-                                                        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-                                                          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+          <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+          <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+          <!--[if lt IE 9]>
+              <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+              <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
                                                             <![endif]-->
 </head>
 
